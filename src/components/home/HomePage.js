15e135a198d32f3971881cba6edd84d13f58de5c
Tien Pham <tienpm1812@gmail.com>
2017-08-19 23:01:03 +0700

    update text

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,8 +5,8 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className="jombotron">
-        <h1>Pluralsight Admin</h1>
-        <p>Hello React Redux</p>
+        <h1>Course Management System</h1>
+        <p>Hello Tien, Hope your have a nice day.</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
       </div>
     );
